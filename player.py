d3032f265f21ac3af6a556ea1bf31e5fe9a35ef7
SobuHasy <sobu.hasy@hotmail.com>
2024-03-07 19:55:59 +0100

    set up the player movement

--- player.py
+++ player.py
@@ -9,7 +9,34 @@ class Player:
         self.angle = PLAYER_ANGLE
 
     def movement(self):
-        pass
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
+        speed = PLAYER_SPEED * self.game.delta_time
+        speed_sin = speed* sin_a
+        speed_cos = speed * cos_a
+
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w]:
+            dx += speed_cos
+            dy += speed_sin
+        if keys[pg.K_s]:
+            dx += -speed_cos
+            dy += -speed_sin
+        if keys[pg.K_a]:
+            dx += speed_sin
+            dy += -speed_cos
+        if keys[pg.K_d]:
+            dx += -speed_sin
+            dy += speed_cos
+
+        if keys[pg.K_LEFT]:
+            self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
+        if keys[pg.K_RIGHT]:
+            self.angle += PLAYER_ROT_SPEED * self.game.delta_time
+        DOUBLE_PI = math.tau
+        self.angle %= DOUBLE_PI
+
 
     def update(self):
         self.movement()
